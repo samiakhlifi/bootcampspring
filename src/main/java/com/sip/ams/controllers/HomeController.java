d2aa2d88a0ff9971fe47c800dab6be55c985ac9c
samiakhlifi <samia.khelifi@polytechnicien.tn>
2022-02-06 15:47:23 +0100

    ex

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
     @RequestMapping("/info")
-    public String info(){
+    public String info(Model model ){
+        String formation="fullstack";
+        ArrayList lists = new ArrayList<>(Arrays.asList("CPA", "CPO", "Spring","Angular"));
+        model.addAttribute("workshop",formation);
+        model.addAttribute("list",lists);
         return "home/info";
     }
 
